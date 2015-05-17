540e60c8e6613ded90eb3fbef2e4e2f415058b84
Mirco Kraenz <prosingularity@yahoo.de>
2015-05-17 10:19:09 +0200

    REBUILD BiggestOfThreeSphere js version

---
+++ laguerre/tools/BiggestOfThreeSpheres.js
@@ -0,0 +1,37 @@
+/**
+ * function definitions
+ */
+function createMidpoints() {
+	ggbApplet.evalCommand('Mo = (10,0,0)');
+	ggbApplet.evalCommand('Oo = (0,0,0)');
+	ggbApplet.evalCommand('Xo = (0,10,3)');
+}
+
+// for ref see https://wiki.geogebra.org/en/Slider_Command
+function createSliders() {
+	ggbApplet.evalCommand('r_{o slider} = Slider[0.1, 10]');
+	ggbApplet.evalCommand('r_{m slider} = Slider[0.1, 10]');
+	ggbApplet.evalCommand('r_{x slider} = Slider[0.1, 10]');
+	
+	ggbApplet.evalCommand('SetValue[r_{o slider}, 1]');
+	ggbApplet.evalCommand('SetValue[r_{m slider}, 1]');
+	ggbApplet.evalCommand('SetValue[r_{x slider}, 1]');
+	
+}
+
+function createSpheres() {
+	ggbApplet.evalCommand('so = Sphere[Oo, r_{o slider}]');
+	ggbApplet.evalCommand('sm = Sphere[Mo, r_{m slider}]');
+	ggbApplet.evalCommand('sx = Sphere[Xo, r_{x slider}]');
+}
+
+/**
+ * main script
+ */
+createMidpoints();
+createSliders();
+createSpheres();
+ggbApplet
+		.evalCommand('bestOfTwoSpheres = If[Radius[so] >= Radius[sm], so, sm]');
+ggbApplet
+		.evalCommand('biggestSphere = If[Radius[bestOfTwoSpheres] >= Radius[sx], bestOfTwoSpheres, sx]');
