b04ae9307bd600db8c4ab1a17e2db427972befa0
Mirco Kraenz <prosingularity@yahoo.de>
2015-05-25 22:16:56 +0200

    FIX #13 working as far as js version worked

--- app/ts/TypeString.ts
+++ app/ts/TypeString.ts
@@ -18,12 +18,34 @@ class TypeString {
     public static midpointString(index: number[]): string {
         return this.typeString('M', index);
     }
-    
+
     public static radiusString(index: number[]): string {
         return this.typeString('r', index);
     }
-    
+
     public static parameterString(index: number[]): string {
-       return this.typeString('parameter', index); 
+        return this.typeString('parameter', index);
+    }
+    
+    /**
+    * 
+    * @param index
+    *            {int-Array} the index as an array in the form [0,1,54,1,-1,1]
+    *            where the last three entries are either 1 or -1
+    * @returns {String} of the form 'midpointRay_{0,1,54,+,+,-}'
+    */
+    public static midpointRayToString(index: number[]): string {
+        var indexCopy: Array<string> = new Array<string>(index.length);
+        for (var i: number = 0; i < index.length; i++) {
+            indexCopy[i] = index[i].toString();
+        }
+        for (var i = 3; i < indexCopy.length; i++) {
+            if (indexCopy[i] == '1') {
+                indexCopy[i] = '+';
+            } else {
+                indexCopy[i] = '-';
+            }
+        }
+        return 'midpointRay_{' + indexCopy.toString() + '}';
     }
 }
