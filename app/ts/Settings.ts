a9cd1f4ea418ca5aff1ce68f34e3238531515d4b
Mirco Kraenz <prosingularity@yahoo.de>
2015-09-27 13:47:17 +0200

    #82: REBUILD RayOfSphereMidpoints.ggt
    - rays that are no parameterrays now start from previous sphere midpoint
    - parameter rays have been adjusted to allow positive radii only

--- app/ts/Settings.ts
+++ app/ts/Settings.ts
@@ -1,14 +1,14 @@
 class Settings {
-    public static MAX_REGION_IN_POSITIVE_X_DIRECTION: number = 4;
-    public static MAX_REGION_IN_POSITIVE_Y_DIRECTION: number = 4;
-    public static MAX_REGION_IN_POSITIVE_Z_DIRECTION: number = 4;
+    public static MAX_REGION_IN_POSITIVE_X_DIRECTION: number = 3;
+    public static MAX_REGION_IN_POSITIVE_Y_DIRECTION: number = 2;
+    public static MAX_REGION_IN_POSITIVE_Z_DIRECTION: number = 3;
 
-    public static MAX_REGION_IN_NEGATIVE_X_DIRECTION: number = 4;
-    public static MAX_REGION_IN_NEGATIVE_Y_DIRECTION: number = 4;
-    public static MAX_REGION_IN_NEGATIVE_Z_DIRECTION: number = 4;
+    public static MAX_REGION_IN_NEGATIVE_X_DIRECTION: number = 1;
+    public static MAX_REGION_IN_NEGATIVE_Y_DIRECTION: number = 1;
+    public static MAX_REGION_IN_NEGATIVE_Z_DIRECTION: number = 1;
 
     // for the parametrizable spheres' slider
-    public static PARAMETER_SPHERE_MIDPOINT_MIN = 0.01;
+    public static PARAMETER_SPHERE_MIDPOINT_MIN = 0.51;
     public static PARAMETER_SPHERE_MIDPOINT_MAX = 0.99;
     public static PARAMETER_SPHERE_MIDPOINT_INCREMENT_STEP = 0.01;
 
