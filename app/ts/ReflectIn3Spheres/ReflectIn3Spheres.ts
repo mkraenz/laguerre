f5a96a39a288c6b0cb0314aae91285a1aa565828
Mirco Kraenz <prosingularity@yahoo.de>
2015-09-28 21:24:32 +0200

    #83 UPDATE ReflectIn3Spheres.ts works

--- app/ts/ReflectIn3Spheres/ReflectIn3Spheres.ts
+++ app/ts/ReflectIn3Spheres/ReflectIn3Spheres.ts
@@ -1,6 +1,6 @@
 function runReflectIn3Spheres() {
     var reflectConstruction = new ReflectIn3Spheres();
-    reflectConstruction.run();
+    reflectConstruction.setUpConstruction();
 }
 
 class ReflectIn3Spheres {
@@ -18,17 +18,30 @@ class ReflectIn3Spheres {
         this.view = new View(this.ggb, this.toStr);
     }
 
-    public run() {
-        var POINT_1 =  'X';
-        var POINT_2 =  'Y';
-        var POINT_3 =  'Z';
-        var planeName = 'reflectionPlane'
-        
+    public setUpConstruction() {
+
+        var POINT_1: string = 'P_1';
+        var POINT_2: string = 'P_2';
+        var POINT_3: string = 'P_3';
+        var GENERIC_PLANE: string = 'tp';
+
+        var MIDPOINT_1: string = 'X';
+        var MIDPOINT_2: string = 'Y';
+        var MIDPOINT_3: string = 'Z';
+        var REFLECTION_PLANE: string = 'mirror';
+
         var util = new Utils(this.ggb, this.t);
-        util.createAbstractedSphereWithSlider(1, 1, 1, 0.25, 0.01, 0.5, 0.01, POINT_1)
-        util.createAbstractedSphereWithSlider(-1, -1, -1, 0.25, 0.01, 0.5, 0.01, POINT_2)
-        util.createAbstractedSphereWithSlider(0, 0, 2, 0.25, 0.01, 0.5, 0.01, POINT_3)
-        
-        this.ggb.plane(POINT_1, POINT_2, POINT_3, planeName);
+
+        util.createAbstractedSphereWithSlider(1, 1, 1, 0.25, 0.01, 0.5, 0.01, MIDPOINT_1);
+        util.createAbstractedSphereWithSlider(-1, -1, -1, 0.25, 0.01, 0.5, 0.01, MIDPOINT_2);
+        util.createAbstractedSphereWithSlider(0, 0, 2, 0.25, 0.01, 0.5, 0.01, MIDPOINT_3);
+
+        this.ggb.plane(MIDPOINT_1, MIDPOINT_2, MIDPOINT_3, REFLECTION_PLANE);
+
+        this.t.pointFree(10, 10, 10, POINT_1);
+        this.t.pointFree(1, 0, 0, POINT_2);
+        this.t.pointFree(2, 2, 2, POINT_3);
+        this.ggb.plane(POINT_1, POINT_2, POINT_3, GENERIC_PLANE);
+        this.ggb.mirror(GENERIC_PLANE, REFLECTION_PLANE, 'mirrorImage');
     }
 }
