ccba124582e95124b8c30d151fd174923e8f6a69
Mirco Kraenz <prosingularity@yahoo.de>
2015-12-03 21:41:06 +0100

    #96: ADD linewise export of data for OBJ vertices
    export midpoints and radii of spheres

--- app/ts/Exporter.ts
+++ app/ts/Exporter.ts
@@ -1,7 +1,9 @@
-var textFile:any = null;
-function makeTextFile(text:any) {
-    var data = new Blob([ text ], {
-        type : 'text/plain'
+// http://stackoverflow.com/questions/21012580/is-it-possible-to-write-data-to-file-using-only-javascript
+// http://jsfiddle.net/UselessCode/qm5AG/
+var textFile: any = null;
+function writeTextFile(text: any) {
+    var data = new Blob([text], {
+        type: 'text/plain'
     });
 
     // If we are replacing a previously generated file we need to
@@ -21,21 +23,28 @@ class Exporter {
     public run() {
         var extractedDataStr: string = this.extractDataFromGGBApplet();
         var link: any = document.getElementById('downloadlink');
-        link.href = makeTextFile(extractedDataStr);
+        link.href = writeTextFile(extractedDataStr);
         link.style.display = 'block';
     }
 
     private extractDataFromGGBApplet() {
         this.toStr = new TypeString();
-        var outputStr: string;
-
-        var midpoint: string = this.toStr.midpoint([0, 0, 2]);
-
-        var xCoord = ggbApplet.getXcoord(midpoint);
-        var yCoord = ggbApplet.getYcoord(midpoint);
-        var zCoord = ggbApplet.getZcoord(midpoint);
-        outputStr = "Coords of M_{0,0,2}: " + xCoord + ',' + yCoord + ',' + zCoord;
+        var outputStr: string = this.toVertexLineInOBJ(this.toStr.midpoint([0, 0, 2]));
         console.log(outputStr);
         return outputStr;
+    }
+
+
+    /** for export to wavefront OBJ format */
+    private toVertexLineInOBJ(pointName: string) {
+        return 'v ' + this.toLineOfCoords(pointName) + '\n';
+    }
+    
+    /** Extracts coordinates of given point to a string of the form "x y z". */
+    private toLineOfCoords(pointName: string): string {
+        var xCoord = ggbApplet.getXcoord(pointName);
+        var yCoord = ggbApplet.getYcoord(pointName);
+        var zCoord = ggbApplet.getZcoord(pointName);
+        return xCoord + ' ' + yCoord + ' ' + zCoord;
     }
 }
