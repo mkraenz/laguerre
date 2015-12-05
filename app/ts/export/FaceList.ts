81b5a22b5e9c4bf3b8419338a983bfd3b5c06544
Mirco Kraenz <prosingularity@yahoo.de>
2015-12-05 11:51:04 +0100

    #97: ADD addFaceInXYPlane
    export intersections of planes to .OBJ

--- app/ts/export/FaceList.ts
+++ app/ts/export/FaceList.ts
@@ -15,6 +15,7 @@ class FaceList extends Array<Face> {
 
     /** adds UP TO 3 faces to this list, a face is added if it is a proper quadrilateral */
     public addUpTo3Faces(x: number, y: number, z: number): void {
+        // NOTE: here and in called methods can be optimized a lot. We run through the whole vertexList way to often.
         if (this.vertexList.isVertexExistent(x, y, z)) {
             this.addFaceInXYPlane(x, y, z);
             this.addFaceInXZPlane(x, y, z);
@@ -25,6 +26,39 @@ class FaceList extends Array<Face> {
     /** adds face [x,y,z], [x+1,y,z], [x+1,y+1,z]. [x,y+1,z] to the list if the face exists, i.e. all
     *   corresponding vertices exist. */
     private addFaceInXYPlane(x: number, y: number, z: number): void {
-        
+        var face = new Face();
+        var v1: Vertex = this.vertexList.getByIndex(x, y, z);
+        var v2: Vertex = this.vertexList.getByIndex(x + 1, y, z);
+        var v3: Vertex = this.vertexList.getByIndex(x + 1, y + 1, z);
+        var v4: Vertex = this.vertexList.getByIndex(x, y + 1, z);
+        if (v1 && v2 && v3 && v4) {
+            face.add4Vertices(v1, v2, v3, v4);
+        }
+    }
+    
+    /** adds face [x,y,z], [x+1,y,z], [x+1,y+1,z]. [x,y+1,z] to the list if the face exists, i.e. all
+    *   corresponding vertices exist. */
+    private addFaceInXZPlane(x: number, y: number, z: number): void {
+        var face = new Face();
+        var v1: Vertex = this.vertexList.getByIndex(x, y, z);
+        var v2: Vertex = this.vertexList.getByIndex(x + 1, y, z);
+        var v3: Vertex = this.vertexList.getByIndex(x + 1, y, z + 1);
+        var v4: Vertex = this.vertexList.getByIndex(x, y, z + 1);
+        if (v1 && v2 && v3 && v4) {
+            face.add4Vertices(v1, v2, v3, v4);
+        }
+    }    
+    
+    /** adds face [x,y,z], [x+1,y,z], [x+1,y+1,z]. [x,y+1,z] to the list if the face exists, i.e. all
+    *   corresponding vertices exist. */
+    private addFaceInYZPlane(x: number, y: number, z: number): void {
+        var face = new Face();
+        var v1: Vertex = this.vertexList.getByIndex(x, y, z);
+        var v2: Vertex = this.vertexList.getByIndex(x, y + 1, z);
+        var v3: Vertex = this.vertexList.getByIndex(x, y + 1, z + 1);
+        var v4: Vertex = this.vertexList.getByIndex(x, y, z + 1);
+        if (v1 && v2 && v3 && v4) {
+            face.add4Vertices(v1, v2, v3, v4);
+        }
     }
 }
