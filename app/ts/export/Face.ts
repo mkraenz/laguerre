69640f40bc50396f442171a1b471556805b100d7
Mirco Kraenz <prosingularity@yahoo.de>
2015-12-05 11:55:43 +0100

    #97: REFACTOR formatting Face
    export intersections of planes to .OBJ

--- app/ts/export/Face.ts
+++ app/ts/export/Face.ts
@@ -7,21 +7,29 @@ class Face {
 
     /** add Vertex to the vertexList, and assert that at most 4 vertices are in the vertexList */
     public addVertex(vertex: Vertex): void {
-        if(this.vertexList.length >= 4){
-           throw new Error('AssertionError: Tried to addVertex() to a Face that already has 4 or more vertices ');
+        if (this.vertexList.length >= 4) {
+            throw new Error('AssertionError: Tried to addVertex() to a Face that already has 4 or more vertices ');
         }
         this.vertexList.push(vertex);
     }
 
     public getVerticesIndexString(): string {
-        if(this.vertexList.length != 4){
-           throw new Error('AssertionError: Tried to getVertexIndexString() of Face without exactly 4 vertices.'); 
+        if (this.vertexList.length != 4) {
+            throw new Error('AssertionError: Tried to getVertexIndexString() of Face without exactly 4 vertices.');
         }
         var outputStr: string = '';
-        for (var i = 0; i<this.vertexList.length; i++){
-           outputStr += this.vertexList[i].getId() + ' ';
+        for (var i = 0; i < this.vertexList.length; i++) {
+            outputStr += this.vertexList[i].getId() + ' ';
         }
         outputStr.trim(); // remove whitespace
         return outputStr;
+    }
+
+    public add4Vertices(v1: Vertex, v2: Vertex, v3: Vertex, v4: Vertex) {
+        // TODO refactor using ...args: Vertex[] as parameter
+        this.addVertex(v1);
+        this.addVertex(v2);
+        this.addVertex(v3);
+        this.addVertex(v4);
     }
 }
