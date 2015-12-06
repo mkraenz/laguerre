8e66e42af748fc46822470d535af7902bec01124
Mirco Kraenz <prosingularity@yahoo.de>
2015-12-06 18:34:04 +0100

    #97: UPDATE vertexList creation checks for well-defined vertices
    export intersections of planes to .OBJ

--- app/ts/export/OBJExporter.ts
+++ app/ts/export/OBJExporter.ts
@@ -16,7 +16,8 @@ class OBJExporter {
             for (var y = -Settings.MAX_REGION_IN_NEG_Y_DIR; y <= Settings.MAX_REGION_IN_POS_Y_DIR; y++) {
                 for (var z = -Settings.MAX_REGION_IN_NEG_Z_DIR; z <= Settings.MAX_REGION_IN_POS_Z_DIR; z++) {
                     var index: number[] = [x, y, z];
-                    if (ggbApplet.exists(this.toStr.planeIntersectionPoint(index))) {
+                    var pointName: string = this.toStr.planeIntersectionPoint(index);
+                    if (ggbApplet.exists(pointName) && ggbApplet.isDefined(pointName)) {
                         vertexList.push(new Vertex(vertexList.length, index, this.toStr));
                     }
                 }
