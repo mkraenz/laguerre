6c27975fa2a91e81bf5d8b313df74cb760c63ec1
Mirco Kraenz <prosingularity@yahoo.de>
2015-12-07 20:43:40 +0100

    #97 VIEW ADD hide labels of intersection points

--- app/ts/export/TPlaneIntersectionPointConstruction.ts
+++ app/ts/export/TPlaneIntersectionPointConstruction.ts
@@ -4,7 +4,11 @@
  */
 class TPlaneIntersectionPointConstruction {
 
-    constructor(private toStr: TypeString, private ggb: GGBTools) { }
+    private view: View;
+
+    constructor(private toStr: TypeString, private ggb: GGBTools) {
+        this.view = new View(ggb, toStr);
+    }
 
     /** intersects the planes tp_{x,0,0}, {0,y,0}, {0,0,z} if they are all well-defined */
     private intersect3Planes(x: number, y: number, z: number): string {
@@ -19,15 +23,17 @@ class TPlaneIntersectionPointConstruction {
 
     public createTPlaneIntersectionPoints() {
         // MAX_REGION +-1 since we need the outer tplanes
-        for (var x = -Settings.MAX_REGION_IN_NEG_X_DIR-1; x <= Settings.MAX_REGION_IN_POS_X_DIR+1; x++) {
-            for (var y = -Settings.MAX_REGION_IN_NEG_Y_DIR-1; y <= Settings.MAX_REGION_IN_POS_Y_DIR+1; y++) {
-                for (var z = -Settings.MAX_REGION_IN_NEG_Z_DIR-1; z <= Settings.MAX_REGION_IN_POS_Z_DIR+1; z++) {
+        for (var x = -Settings.MAX_REGION_IN_NEG_X_DIR - 1; x <= Settings.MAX_REGION_IN_POS_X_DIR + 1; x++) {
+            for (var y = -Settings.MAX_REGION_IN_NEG_Y_DIR - 1; y <= Settings.MAX_REGION_IN_POS_Y_DIR + 1; y++) {
+                for (var z = -Settings.MAX_REGION_IN_NEG_Z_DIR - 1; z <= Settings.MAX_REGION_IN_POS_Z_DIR + 1; z++) {
                     if (x != 0 && y != 0 && z != 0) {
-                        this.intersect3Planes(x, y, z);
+                        var interSectionPointName: string = this.intersect3Planes(x, y, z);
+                        this.view.listOfInvisibleLabels.push(interSectionPointName);
                     }
                 }
             }
         }
+        this.view.setLabelsInvisible();
     }
 
 }
