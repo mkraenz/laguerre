93eb340795a3bfe1298b08250c83ff2ba8232bdb
Mirco Kraenz <prosingularity@yahoo.de>
2015-12-12 18:11:07 +0100

    #105: REMOVE Face.name
    Draw Faces

--- app/ts/export/Face.ts
+++ app/ts/export/Face.ts
@@ -2,12 +2,6 @@ class Face {
     private vertexList: VertexList = new VertexList();
     private name: string;
 
-    constructor(private toStr: TypeString) { }
-
-    public getName() {
-        return this.name;
-    }
-
     public toFaceLineInOBJ(): string {
         return 'f ' + this.getVerticesIndexString() + '\n';
     }
@@ -38,12 +32,11 @@ class Face {
         this.addVertex(v2);
         this.addVertex(v3);
         this.addVertex(v4);
-        this.name = this.toStr.face(v1.getIndex(), v2.getIndex(), v3.getIndex(), v4.getIndex());
     }
 
     public createFaceInGGB(): void {
         var tools = new Tools(new TypeString(), new GGBTools());
         tools.quad(this.vertexList[0].getIndex(), this.vertexList[1].getIndex(),
-            this.vertexList[2].getIndex(), this.vertexList[3].getIndex(), this.name);
+            this.vertexList[2].getIndex(), this.vertexList[3].getIndex());
     }
 }
