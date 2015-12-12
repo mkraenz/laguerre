a9d9d922ee99d9d15b912a4ebb908bfeed6ac3fd
Mirco Kraenz <prosingularity@yahoo.de>
2015-12-12 15:49:05 +0100

    #106: REMOVE addFaceIn**Plane DOC FaceList
    Refactor Exports addInBlaPlane()

--- app/ts/export/FaceList.ts
+++ app/ts/export/FaceList.ts
@@ -22,7 +22,9 @@ class FaceList extends Array<Face> {
             this.addFaceInPlane(x, y, z, 2); // xy
         }
     }
-
+    
+    /** adds a face like [x,y,z], [x+1,y,z], [x+1,y+1,z]. [x,y+1,z] (example for normalDir == 0) to the list if the face exists,
+    * i.e. all corresponding vertices exist. */
     private addFaceInPlane(x: number, y: number, z: number, normalDir: number): void {
         var indexMatrix = this.getIndexMatrix(x, y, z, normalDir);
         var v1: Vertex = this.vertexList.getVertexByIndex(indexMatrix[0]);
@@ -31,7 +33,8 @@ class FaceList extends Array<Face> {
         var v4: Vertex = this.vertexList.getVertexByIndex(indexMatrix[3]);
         this.addFace(v1, v2, v3, v4);
     }
-
+    
+    /** creates a matrix like [ [x,y,z], [x+1,y,z], [x+1,y+1,z]. [x,y+1,z] ] (example for normalDir == 0)*/
     private getIndexMatrix(x: number, y: number, z: number, normalDir: number): number[][] {
         var indexMatrix: number[][] = [[x, y, z], [x, y, z], [x, y, z], [x, y, z]];
         var shiftMatrix: number[][] = this.getShiftMatrix(x, y, z, normalDir);
@@ -39,6 +42,7 @@ class FaceList extends Array<Face> {
         return matrix;
     }
 
+    /** see photos 2015-12-10 */
     private getShiftMatrix(x: number, y: number, z: number, normalDir: number): number[][] {
         var shiftMatrix: number[][] = Utils.getShiftMatrix();
         var mathjsExt: MathjsExtensions = new MathjsExtensions();
@@ -49,36 +53,6 @@ class FaceList extends Array<Face> {
         return shiftMatrix;
     }
     
-    /** adds face [x,y,z], [x+1,y,z], [x+1,y+1,z]. [x,y+1,z] to the list if the face exists, i.e. all
-    *   corresponding vertices exist. */
-    private addFaceInXYPlane(x: number, y: number, z: number): void {
-        var v1: Vertex = this.vertexList.getByIndex(x, y, z);
-        var v2: Vertex = this.vertexList.getByIndex(x + this.oneOrTwo(x), y, z);
-        var v3: Vertex = this.vertexList.getByIndex(x + this.oneOrTwo(x), y + this.oneOrTwo(y), z);
-        var v4: Vertex = this.vertexList.getByIndex(x, y + this.oneOrTwo(y), z);
-        this.addFace(v1, v2, v3, v4);
-    }
-    
-    /** adds face [x,y,z], [x+1,y,z], [x+1,y+1,z]. [x,y+1,z] to the list if the face exists, i.e. all
-    *   corresponding vertices exist. */
-    private addFaceInXZPlane(x: number, y: number, z: number): void {
-        var v1: Vertex = this.vertexList.getByIndex(x, y, z);
-        var v2: Vertex = this.vertexList.getByIndex(x + this.oneOrTwo(x), y, z);
-        var v3: Vertex = this.vertexList.getByIndex(x + this.oneOrTwo(x), y, z + this.oneOrTwo(z));
-        var v4: Vertex = this.vertexList.getByIndex(x, y, z + this.oneOrTwo(z));
-        this.addFace(v1, v2, v3, v4);
-    }    
-    
-    /** adds face [x,y,z], [x+1,y,z], [x+1,y+1,z]. [x,y+1,z] to the list if the face exists, i.e. all
-    *   corresponding vertices exist. */
-    private addFaceInYZPlane(x: number, y: number, z: number): void {
-        var v1: Vertex = this.vertexList.getByIndex(x, y, z);
-        var v2: Vertex = this.vertexList.getByIndex(x, y + this.oneOrTwo(y), z);
-        var v3: Vertex = this.vertexList.getByIndex(x, y + this.oneOrTwo(y), z + this.oneOrTwo(z));
-        var v4: Vertex = this.vertexList.getByIndex(x, y, z + this.oneOrTwo(z));
-        this.addFace(v1, v2, v3, v4);
-    }
-
     private oneOrTwo(n: number): number {
         if (n == -1) {
             return 2;
