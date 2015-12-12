3ef7977b366bdbe910587b5fc3e4bc543470b479
Mirco Kraenz <prosingularity@yahoo.de>
2015-12-12 14:41:42 +0100

    #106: REMOVE test code from MathjsExtensions
    Refactor Exports addInBlaPlane()

--- app/ts/utils/MathjsExtensions.ts
+++ app/ts/utils/MathjsExtensions.ts
@@ -7,18 +7,4 @@ class MathjsExtensions {
             matrix[i][columnIndex2] = tmp;
         }
     }
-
-    private addFaceInPlane(x: number, y: number, z: number, normalDir: number): void {
-        // TODO
-        var indexMatrix: number[][] = [[x, y, z], [x, y, z], [x, y, z], [x, y, z]]
-        var mathjsExt: MathjsExtensions = new MathjsExtensions();
-        var shiftMatrix: number[][] = Utils.getShiftMatrix();
-        mathjsExt.switchMatrixColumns(shiftMatrix, 0, normalDir);
-        var resultingMatrix = math.add(indexMatrix, shiftMatrix);
-        console.log('resulting matrix = ' + resultingMatrix);
-    }
-
-    public testSwitchMatrixColumns() {
-        this.addFaceInPlane(0, 0, 0, 1);
-    }
 }
