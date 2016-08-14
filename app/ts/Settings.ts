b5bc6fdd1ebaef76917f59500f12531d54cc3a0b
Mirco <Mirco@Mirco-PC>
2016-08-14 17:30:26 +0200

    #110 CONFIG smaller range for sphere radii

--- app/ts/Settings.ts
+++ app/ts/Settings.ts
@@ -1,15 +1,15 @@
 class Settings {
-    public static MAX_REGION_IN_POS_X_DIR: number = 2;
-    public static MAX_REGION_IN_POS_Y_DIR: number = 2;
-    public static MAX_REGION_IN_POS_Z_DIR: number = 2;
+    public static MAX_REGION_IN_POS_X_DIR: number = 3;
+    public static MAX_REGION_IN_POS_Y_DIR: number = 3;
+    public static MAX_REGION_IN_POS_Z_DIR: number = 3;
 
-    public static MAX_REGION_IN_NEG_X_DIR: number = 2;
-    public static MAX_REGION_IN_NEG_Y_DIR: number = 2;
-    public static MAX_REGION_IN_NEG_Z_DIR: number = 2;
+    public static MAX_REGION_IN_NEG_X_DIR: number = 3;
+    public static MAX_REGION_IN_NEG_Y_DIR: number = 3;
+    public static MAX_REGION_IN_NEG_Z_DIR: number = 3;
 
     // for the parametrizable spheres' slider
-    public static PARAMETER_SPHERE_MIDPOINT_MIN = 0.01;
-    public static PARAMETER_SPHERE_MIDPOINT_MAX = 0.99;
+    public static PARAMETER_SPHERE_MIDPOINT_MIN = 0.4;
+    public static PARAMETER_SPHERE_MIDPOINT_MAX = 0.6;
     public static PARAMETER_SPHERE_MIDPOINT_INCREMENT_STEP = 0.01;
 
     public static ORIGIN_SPHERE_SCALING = 0.3;
