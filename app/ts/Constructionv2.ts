ca685e121412aeb5d0f11fe4c4549d7ed3871d9a
Mirco Kraenz <prosingularity@yahoo.de>
2017-05-10 18:52:13 +0200

    #82: REFACTOR unite construction in pos and neg z direction
    MidpointRays don't intersect each other

--- app/ts/Constructionv2.ts
+++ app/ts/Constructionv2.ts
@@ -221,15 +221,20 @@ class Construction2 {
     }
 
     private constructInZDirection(): void {
-        this.constructInPositiveZDirection();
+        this.constructInSomeZDirection(Settings.MAX_REGION_IN_POS_Z_DIR, true)
+//        this.constructInSomeZDirection(Settings.MAX_REGION_IN_NEG_Z_DIR, false)
         //        this.constructInNegativeZDirection();
         //        this.constructFourthSpheresInBothZDirections();
     }
 
-    private constructInPositiveZDirection(): void {
+    private constructInSomeZDirection(max_z: number, direction: boolean): void {
+        /**
+         * if direction == true then positive direction, else negative direction
+         */
+        var sign: number = direction ? +1 : -1
         var x: number = 0;
         var y: number = 0;
-        for (var z = 1; z < Settings.MAX_REGION_IN_POS_Z_DIR; z++) {
+        for (var z = 1; z < max_z; z++) {
             var targetRegions1: Array<number[]>;
             var targetRegions2: Array<number[]>;
             var startRegion1: number[];
@@ -237,10 +242,10 @@ class Construction2 {
             var startRegions1: Array<number[]>;
             var startRegions2: Array<number[]>;
 
-            if (z % 2 == 1) {
+            if (Math.abs(z) % 2 == 1) {
                 // startRegions1 = spheres in combinatorics_in_tp_100_from_3_spheres.ggb
                 // targetRegions1 = spheres in example_combinatorics_in_tp_200_from_4_spheres.ggb
-                targetRegions1 = [[x, y, z + 1], [x + 2, y, z + 1], [x, y + 2, z + 1]];
+                targetRegions1 = [[x, y, z + 1*sign], [x + 2, y, z + 1*sign], [x, y + 2, z + 1*sign]];
                 startRegion1 = [x + 1, y + 1, z];
                 startRegions1 = [[x + 1, y - 1, z], [x + 1, y - 1, z], [x - 1, y + 1, z]];
 
@@ -249,13 +254,13 @@ class Construction2 {
                  * which are not in combinatorics_in_tp_100_from_3_spheres.ggb
                  * targetRegions = remaining 2s spheres in example_combinatorics_in_tp_100_from_4_spheres.ggb 
                  */
-                targetRegions2 = [[x - 2, y, z + 1], [x, y - 2, z + 1]];
+                targetRegions2 = [[x - 2, y, z + 1*sign], [x, y - 2, z + 1*sign]];
                 startRegion2 = [x - 1, y - 1, z];
                 startRegions2 = [[x - 1, y + 1, z], [x + 1, y - 1, z]]
             }
             else {
-                targetRegions1 = [[x + 1, y + 1, z + 1], [x + 1, y - 1, z + 1], [x - 1, y + 1, z + 1],
-                    [x - 1, y - 1, z + 1]];
+                targetRegions1 = [[x + 1, y + 1, z + 1*sign], [x + 1, y - 1, z + 1*sign],
+                     [x - 1, y + 1, z + 1*sign], [x - 1, y - 1, z + 1*sign]];
                 startRegion1 = [x, y, z];
                 startRegions1 = [[x + 2, y, z], [x + 2, y, z], [x - 2, y, z], [x - 2, y, z]];
             }
