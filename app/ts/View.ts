a59fa55b0c89d4947a2a2a613d52e355148a40b4
Mirco Kraenz <prosingularity@yahoo.de>
2017-07-31 13:43:42 +0200

    #123 ADD hide non-inspheres

--- app/ts/View.ts
+++ app/ts/View.ts
@@ -40,18 +40,17 @@ class View {
     }
 
     private setColorOfnthOrderSphere(order: number, color: string): void {
-        // Starting at 0. 0 colors the origin sphere, then proceed outwards by steps of 1
+        // Starting at order 0. 0 colors the origin sphere, then proceed outwards by steps of 1
         if (order < 0) {
             throw new Error('Illegal Argument: Order must be greater or equal to zero. Given parameter: order = '
                 + order);
         }
-        var toStr: TypeString = new TypeString();
         for (var i: number = -order; i <= order; i++) {
             for (var j: number = -order; j <= order; j++) {
                 var targetRegions: Array<number[]> = [[-order, i, j], [order, i, j], [i, -order, j], [i, order, j],
                     [i, j, -order], [i, j, order]];
                 for (var k: number = 0; k < targetRegions.length; k++) {
-                    if (ggbApplet.exists(toStr.sphere(targetRegions[k]))) {
+                    if (ggbApplet.exists(this.toStr.sphere(targetRegions[k]))) {
                         this.setColorOfSphere(targetRegions[k], color);
                     }
                 }
@@ -61,5 +60,34 @@ class View {
 
     private setColorOfSphere(targetRegion: number[], color: string): void {
         this.ggb.setColor(this.toStr.sphere(targetRegion), color);
+    }
+
+    public hideSphere(regionIndex: number[]): void {
+        if (ggbApplet.exists(this.toStr.sphere(regionIndex))) {
+            ggbApplet.setVisible(this.toStr.sphere(regionIndex), false);
+        }
+    }
+
+    public hideallNonInspheres(): void {
+
+        for (var y: number = -Settings.MAX_REGION_IN_NEG_Y_DIR; y <= Settings.MAX_REGION_IN_POS_Y_DIR; y++) {
+            for (var z: number = -Settings.MAX_REGION_IN_NEG_Z_DIR; z <= Settings.MAX_REGION_IN_POS_Z_DIR; z++) {
+
+                this.hideSphere([-Settings.MAX_REGION_IN_NEG_X_DIR - 1, y, z])
+                this.hideSphere([Settings.MAX_REGION_IN_POS_X_DIR + 1, y, z])
+            }
+            for (var x: number = -Settings.MAX_REGION_IN_NEG_X_DIR; x <= Settings.MAX_REGION_IN_POS_X_DIR; x++) {
+                this.hideSphere([x, y, -Settings.MAX_REGION_IN_NEG_Z_DIR - 1])
+                this.hideSphere([x, y, Settings.MAX_REGION_IN_POS_Z_DIR + 1])
+            }
+        }
+
+        for (var x: number = -Settings.MAX_REGION_IN_NEG_X_DIR; x <= Settings.MAX_REGION_IN_POS_X_DIR; x++) {
+            for (var z: number = -Settings.MAX_REGION_IN_NEG_Z_DIR; z <= Settings.MAX_REGION_IN_POS_Z_DIR; z++) {
+                this.hideSphere([x, -Settings.MAX_REGION_IN_NEG_Y_DIR - 1, z])
+                this.hideSphere([x, Settings.MAX_REGION_IN_POS_Y_DIR + 1, z])
+
+            }
+        }
     }
 }
